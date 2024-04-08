e5485d0fb384cf4fd40e4a1d2d2f2784b84a9719
Hariprasath <truebillionhari@gmail.com>
2024-04-08 14:20:54 +0530

    endpoint-2

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import router from "./router/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.use('/ping',router)
+app.use("/ping", router);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
